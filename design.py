375b4d2927dd4a9ca27ec615e2a00928beebd5bf
Anton-beep <anton.plyu@gmail.com>
2022-01-31 19:15:55 +0300

    fix

--- design.py
+++ design.py
@@ -2,6 +2,7 @@ import os
 import api
 
 from PyQt5 import uic
+from PyQt5.QtCore import Qt
 from PyQt5.QtWidgets import QMainWindow
 from PyQt5.QtGui import QPixmap
 from PyQt5 import QtCore
@@ -24,14 +25,6 @@ class MainWindow(QMainWindow):
     def initUI(self):
         self.Update()
 
-    def keyPressEvent(self, event):
-        if event.key() == QtCore.Qt.Key_PageUp:
-            self.z_scale = min(17, self.z_scale + 1)
-        if event.key() == QtCore.Qt.Key_PageDown:
-            self.z_scale -= min(1, self.z_scale - 1)
-
-        self.Update()
-
     def TypeMapChanger(self):
         self.map_type = self.laymap.currentText()
         if self.map_type == 'Satellite':
@@ -49,6 +42,28 @@ class MainWindow(QMainWindow):
         self.laymap.currentIndexChanged.connect(self.TypeMapChanger)
         self.Zooml.setText(f'Zoom: {str(self.z_scale / 9 * 100)[:3]}%')
         self.Coordsl.setText(f'Coords: {self.coords}')
+
+    def keyPressEvent(self, event):
+        # zoom
+        if event.key() == QtCore.Qt.Key_PageUp:
+            self.z_scale = min(17, self.z_scale + 1)
+        if event.key() == QtCore.Qt.Key_PageDown:
+            self.z_scale -= min(1, self.z_scale - 1)
+
+        # moving
+        coords = list(map(float, self.coords.split(',')))
+        value = 0.02
+        if event.key() in [Qt.Key_Up, Qt.Key_W]:
+            coords[1] += value
+        if event.key() in [Qt.Key_Left, Qt.Key_A]:
+            coords[0] -= value
+        if event.key() in [Qt.Key_Down, Qt.Key_S]:
+            coords[1] -= value
+        if event.key() in [Qt.Key_Right, Qt.Key_D]:
+            coords[0] += value
+
+        self.coords = f'{str(coords[0])[:6]},{str(coords[1])[:6]}'
+        self.Update()
 
     def closeEvent(self, event):
         """При закрытии формы подчищаем за собой"""
